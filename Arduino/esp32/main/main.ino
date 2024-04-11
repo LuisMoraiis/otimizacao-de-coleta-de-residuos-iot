c35a45dcab53babb2985846e735b539336e471ff
Luis Morais <fnluismoraiis.750@gmail.com>
2024-04-11 12:49:32 -0300

    Adição de modulo: segundo sensor

--- Arduino/esp32/main/main.ino
+++ Arduino/esp32/main/main.ino
@@ -1,17 +1,11 @@
 #include <Arduino.h>
 
-const int trigPin = 4;
-const int echoPin = 2;
+const int trigPin_1 = 4;
+const int echoPin_1 = 2;
+const int trigPin_2 = 18;
+const int echoPin_2 = 5;
 
-void setup() {
-  Serial.begin(9600);
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-}
-
-void loop() {
-
+void defaultHC_SR04(int trigPin, int echoPin, int numSensor){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -21,10 +15,26 @@ void loop() {
   long duration = pulseIn(echoPin, HIGH);
 
   long distance = duration * 0.034 / 2;
-
-  Serial.print("Distance: ");
+  Serial.print("Sensor ");
+  Serial.print(numSensor);
+  Serial.print("  Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(trigPin_1, OUTPUT);
+  pinMode(echoPin_1, INPUT);
+  pinMode(trigPin_2, OUTPUT);
+  pinMode(echoPin_2, INPUT);
+}
+
+void loop() {
+
+  defaultHC_SR04(trigPin_1, echoPin_1, 1);
+  defaultHC_SR04(trigPin_2, echoPin_2, 2);
 
   delay(1000);
 
